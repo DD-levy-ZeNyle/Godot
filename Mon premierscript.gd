fed2b901a075ca7684ca9a3e5701cd7428453f80
Yoanl <itouayohann31@gmail.com>
2026-05-24 17:04:03 +0100

    1er_les_fonctions
    Mise en place de la fonction simple dire bonjour avec pour appel de cette fonction dans la fonction ready

--- Mon premierscript.gd
+++ Mon premierscript.gd
@@ -2,16 +2,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
-	pass
+	dire_bonjour()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_right"):
-		print("Flèche de droite maintenue")
-	elif Input.is_action_just_pressed("ui_left"):
-		print("la flèche de gauche est maintenue")
-	elif Input.is_action_just_pressed("ui_up"):
-		print("la flèche du haut est maintenue")
-	elif Input.is_action_just_pressed("ui_down"):
-		print("la flèche du bas est maintenue")
+	pass
+
+func dire_bonjour():
+	print("Salut salut!!!")
