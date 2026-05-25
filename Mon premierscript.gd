67b44d804515ae7e447e51d65478d112fe1c0ef9
Yoanl <itouayohann31@gmail.com>
2026-05-25 12:16:51 +0100

    Utilisation du nœud label fonctionnel en ligne de code
    Dnas les anciens tests j'ai remarqué que le nœud label créé par interface n'était pas fonctionnel donc je l'ai réalisé par ligne de code le souci c'est que tout modification se fera par code par la suite

--- Mon premierscript.gd
+++ Mon premierscript.gd
@@ -2,7 +2,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$textes.text =("Bonjour, godot !!!")
+	var texte = Label.new()
+	texte.text = "Texte ajouté par code"
+	add_child(texte)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
