78d1bd19643cc6ba4ad4e20f4b36725005efcc10
Yoanl <itouayohann31@gmail.com>
2026-05-25 10:30:00 +0100

    2eme les fonctions
    Forme plus simple d'appel de fonction

--- Mon premierscript.gd
+++ Mon premierscript.gd
@@ -2,12 +2,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
-	dire_bonjour()
+	dire ("Bonjour")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func dire_bonjour():
-	print("Salut salut!!!")
+func dire (message):
+	print(message)
